fc07a9fa21b3a8724652b47a51b6cfef16d7c589
peter.zhong <peter.zhong@meetsocial.com>
2026-07-07 08:43:02 +0800

    ci: add GitHub Actions workflow

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    ignores: ["dist", "node_modules"],
+  },
+];
